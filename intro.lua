bebfb701fa5f9d353a15b8cef8d9528b103ccb9e
nexsync <156602124+NexSync-dev@users.noreply.github.com>
2026-06-07 09:18:33 +0000

    Update intro.lua

--- intro.lua
+++ intro.lua
@@ -1,12 +1,1 @@
-local NotificationLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/NexSync-dev/neverlose/refs/heads/main/message.lua"))()
 
--- Example Usage:
-NotificationLib:Notify("welcome to NexSync hub", "info")(2)
-task.wait(1)
-NotificationLib:Notify("Loading GUI", "info")(3)
-task.wait(1)
-NotificationLib:Notify("This may not be as good as you expect", "warning")(4)
-task.wait(1)
-NotificationLib:Notify("If something doesnt work after dying,try disabling and re-enabling it or fully re-executing", "warning")(5)
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NexSync-dev/neverlose/refs/heads/main/test.lua"))()
